7743e0d2e362db00c62595c27ddaf66904ed5f25
Walter Fritsche <wfritsche95@protonmail.com>
2023-11-09 13:23:41 -0600

    added slugs field

--- models/category.js
+++ models/category.js
@@ -1,14 +1,14 @@
 const mongoose = require("mongoose");
-const slugify = require("slugify");
 const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
   name: { type: String, required: true },
   description: String,
+  slug: { type: String, required: true },
 });
 
 CategorySchema.virtual("url").get(function () {
-  return `/category/${slugify(this.name, { lower: true })}`;
+  return `/category/${this.slug}`;
 });
 
 module.exports = mongoose.model("Category", CategorySchema);

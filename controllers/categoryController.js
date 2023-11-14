8dd25fa8839831207c29f085604db8ab93d66c8d
Walter Fritsche <wfritsche95@protonmail.com>
2023-11-14 10:57:25 -0600

    implemented category delete GET and POST

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -113,15 +113,63 @@ exports.category_edit_post = [
 ];
 
 // GET /category/:categoryName/delete
-exports.category_delete_get = asyncHandler(async (req, res) => {
-  res.send(
-    `Route not implemented: confirmation page to delete ${req.params.categoryName} category`,
-  );
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+  const category = await Category.findOne({ slug: req.params.categoryName });
+  const products = await Instrument.find({ category: category });
+
+  if (category === null) {
+    const err = new Error("Category not found");
+    err.status = 404;
+    return next(err);
+  }
+  if (products.length > 0) {
+    res.render("category_delete", {
+      title: `Delete ${category.name}?`,
+      category: category,
+      products: products,
+    });
+  } else {
+    res.render("category_delete", {
+      title: `Delete ${category.name}?`,
+      category: category,
+      products: undefined,
+    });
+  }
 });
 
 // POST /category/:categoryName/delete
-exports.category_delete_post = asyncHandler(async (req, res) => {
-  res.send(
-    `Route not implemented: post response to delete ${req.params.categoryName} category`,
-  );
-});
+exports.category_delete_post = [
+  body("categoryid").trim().isMongoId(),
+  asyncHandler(async (req, res, next) => {
+    const errResult = validationResult(req);
+    const data = matchedData(req);
+
+    const category = await Category.findById(data.categoryid).exec();
+    const products = await Instrument.find({ category: category });
+    if (category === null) {
+      const err = new Error("Category doesn't exist");
+      err.status = 500;
+      return next(err);
+    }
+    if (errResult.isEmpty()) {
+      // no errors
+      if (products.length > 0) {
+        // There are still products under that category
+        res.render("category_delete", {
+          title: `You can't delete ${category.name}`,
+          category: category,
+          products: products,
+        });
+      } else {
+        // the category exists but there are not products under it.
+        await Category.findByIdAndDelete(data.categoryid).exec();
+        res.redirect("/categories");
+      }
+    } else {
+      // errors in validation
+      const err = new Error("Unknown error occured");
+      err.status = 500;
+      return next(err);
+    }
+  }),
+];

8df286792e670cfaeda6c031723e9a544980156c
Walter Fritsche <wfritsche95@protonmail.com>
2023-11-16 10:16:56 -0600

    implemented product creation and adujsted models, routes, and views to fit new parameters

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,68 @@
 const asyncHandler = require("express-async-handler");
 const Category = require("../models/category");
 const Instrument = require("../models/instrument");
+const { body, validationResult, matchedData } = require("express-validator");
 
 // GET /product/create
-exports.product_create_get = (req, res) => {
-  res.send("Route not implemented: send a create new product form");
-};
-
-// POST /product/create
-exports.product_create_post = asyncHandler(async (req, res) => {
-  res.send("Route not implemented: send a create new product form");
+exports.product_create_get = asyncHandler(async (req, res) => {
+  const categories = await Category.find({}).exec();
+  res.render("product_form", {
+    title: "Create A New Product",
+    categories: categories,
+    errors: undefined,
+    selectedCategory: undefined,
+  });
 });
 
-// GET /product/:productId
+// POST /product/create
+exports.product_create_post = [
+  body("name").trim().isLength({ max: 140 }).escape(),
+  body("description").optional().trim().escape(),
+  body("category").isMongoId().escape(),
+  body("price").trim().isFloat({ gt: 0 }).escape(),
+  body("instock").trim().isInt({ min: 0 }).escape(),
+  body("slug").trim().isSlug().escape(),
+  asyncHandler(async (req, res) => {
+    const errResult = validationResult(req);
+    const data = matchedData(req);
+
+    if (errResult.isEmpty()) {
+      // no errors
+      const category = await Category.findById(data.category).exec();
+      const product = new Instrument({
+        name: data.name,
+        description: data.description,
+        category: category,
+        price: data.price,
+        inStock: data.instock,
+        slug: data.slug,
+      });
+      product.save();
+      res.redirect(product.url);
+    } else {
+      // errors in validation
+      const categories = await Category.find({}).exec();
+      res.render("product_form", {
+        title: "Add a product",
+        selectedCategory: data.category ?? "",
+        product: {
+          name: data.name,
+          description: data.description,
+          price: data.price,
+          inStock: data.instock,
+          slug: data.slug,
+        },
+        errors: errResult,
+        categories: categories,
+      });
+    }
+  }),
+];
+
+// GET /product/:productSlug
 exports.product_detail = asyncHandler(async (req, res, next) => {
   try {
-    const product = await Instrument.findById(req.params.productId)
+    const product = await Instrument.findOne({ slug: req.params.productSlug })
       .populate("category")
       .exec();
     if (product) {
@@ -38,30 +85,30 @@ exports.product_detail = asyncHandler(async (req, res, next) => {
   }
 });
 
-// GET /product/:productId/edit
+// GET /product/:productSlug/edit
 exports.product_edit_get = asyncHandler(async (req, res) => {
   res.send(
-    `Route not implemented: prefilled product form to edit: ${req.params.productId}`,
+    `Route not implemented: prefilled product form to edit: ${req.params.productSlug}`,
   );
 });
 
-// POST /product/:productId/edit
+// POST /product/:productSlug/edit
 exports.product_edit_post = asyncHandler(async (req, res) => {
   res.send(
-    `Route not implemented: post response to edit product: ${req.params.productId}`,
+    `Route not implemented: post response to edit product: ${req.params.productSlug}`,
   );
 });
 
-// GET /product/:productId/delete
+// GET /product/:productSlug/delete
 exports.product_delete_get = asyncHandler(async (req, res) => {
   res.send(
-    `Route not implemented: confirmation page to delete: ${req.params.productId}`,
+    `Route not implemented: confirmation page to delete: ${req.params.productSlug}`,
   );
 });
 
-// POST /product/:productId/delete
+// POST /product/:productSlug/delete
 exports.product_delete_post = asyncHandler(async (req, res) => {
   res.send(
-    `Route not implemented: post response to delete product: ${req.params.productId}`,
+    `Route not implemented: post response to delete product: ${req.params.productSlug}`,
   );
 });
--- routes/products.js
+++ routes/products.js
@@ -7,14 +7,14 @@ router.get("/create", productController.product_create_get);
 
 router.post("/create", productController.product_create_post);
 
-router.get("/:productId", productController.product_detail);
+router.get("/:productSlug", productController.product_detail);
 
-router.get("/:productId/edit", productController.product_edit_get);
+router.get("/:productSlug/edit", productController.product_edit_get);
 
-router.post("/:productId/edit", productController.product_edit_post);
+router.post("/:productSlug/edit", productController.product_edit_post);
 
-router.get("/:productId/delete", productController.product_delete_get);
+router.get("/:productSlug/delete", productController.product_delete_get);
 
-router.post("/:productId/delete", productController.product_delete_post);
+router.post("/:productSlug/delete", productController.product_delete_post);
 
 module.exports = router;
